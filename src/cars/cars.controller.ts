4570112100f8646c9588cf1af009d68e5e782315
BryanGallo <bryangallo95@hotmail.com>
2024-08-17 11:18:33 -0500

    Creacion cars.service.ts e inyeccion de dependencia
    * El servide aloja la logica de negocio y se conecta con la bdd
    * Gererando arreglo de cars y metodos para obtner los cars o car
    * * En este caso el arreglo cars representa la base
    * Inyectando dependencias en cars.controller.ts a traves del constructor

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,17 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['Chery', 'Haval', 'Jeep'];
+  constructor(private readonly carsService: CarsService) {}
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
   getCarById(@Param('id') id: string) {
-    console.log(id);
-
-    return this.cars[id];
+    return this.carsService.findOne(+id);
   }
 }
